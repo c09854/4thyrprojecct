e1213f18d735499e61a6f648b56e98eac09dee14
martin <c09854151@mydit.ie>
2012-11-27 17:53:13 +0000

    mostly finished logic for pokergame display

--- app/helpers/pokergame_helper.rb
+++ app/helpers/pokergame_helper.rb
@@ -1,8 +1,8 @@
 module PokergameHelper
     
-    class card
+    class Card
         
-        def initilize(number,suit)
+        def initialize(number,suit)
             @number = number
             @suit = suit
         end
@@ -14,10 +14,6 @@ module PokergameHelper
         def number
             @number
         end
-        
-        def card
-            [@number,@suit]
-        end
     end 
     
     def generate_deck_of_cards
@@ -28,10 +24,11 @@ module PokergameHelper
         suits = ["c","s","h","d"]
         suits.each do |s|
             (1..13).each do |i|
-                x = i
-                #card = Card.new(x,s)
-                #@pokerdeck << card
-                @pokerdeck << [x,i]
+                suit = s
+                number = i
+                card = Card.new(number,suit)
+                @pokerdeck << card
+               # @pokerdeck << [x,i]
             end
         end
     end
@@ -50,6 +47,7 @@ module PokergameHelper
         @eg_users.each do |user|
             user << @pokerdeck.pop
         end
+        @eg_users
     end
     
     
@@ -84,32 +82,205 @@ module PokergameHelper
     def winner
         @best_hand = {}
         @eg_users.each do |user|
-           #require 'debugger' ; debugger
             @usr = user[0]
             @best_hand[@usr] = []
             cards = []
-            cards = cards + @communal_cards
+            cards = Array.new(@communal_cards)
             cards << user[1]
             cards << user[2]
-            calculate_hand cards,@usr
+            calculate_hand cards
         end
-        determine_winner
+       best_hnd_key = determine_winner
+    end
+    
+    def determine_winner
+        num = 10
+        best_hnd_key = ""
+        hand_list = {"straight flush" => 1,"four of a kind" => 2,"fullhouse" => 3,"flush" => 4,"straight" =>5,"three of a kind" =>6,"two pair" =>7,"pair" => 8,"high card" => 9}
+        @best_hand.each do |key,value|
+            hand = value.pop
+            if num > hand_list[hand]
+                num = hand_list[hand] 
+                best_hnd_key = key
+            end
+        end
+        best_hnd_key
     end
     
     # calculates hand 
     def calculate_hand cards
-       # d,s,h,c,ace,two,three,four,five,six,seven,eight,nine,ten,jack,queen,king = 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-        hand_hsh
+        straight? cards
+        hand_hsh = {}
+        suit_hsh = {}
         cards.each do |card|
             determine_cards(card,hand_hsh)
+            determine_suits(card,suit_hsh)
         end
-        get_hand hand_hsh
+        get_best_hand hand_hsh,suit_hsh
     end
     
+    # common method for assigning both card value and suit to hashes. called in determine suit, determine facevalue
+    def assign_to_hsh hsh,card,key 
+        hsh[key] = [] unless hsh.has_key? key
+        hsh[key] << card
+    end
+    
+        
+    def get_best_hand hand_hsh,suit_hsh
+        win_arr = []
+        num = 5
+        hand = ""
+        win_hsh = suit_hsh.select {|key, value| value.length == num};   
+        unless win_hsh.empty?
+            hand = "flush" 
+            win_arr = win_hsh[win_hsh.keys[0]]
+        
+            str_arr = straight? win_arr
+            if str_arr.length == 5
+                win_arr = str_arr
+                hand = "straight flush"
+                win_arr << hand
+            end
+        end
+        win_hsh.clear
+        if hand != "straight flush"
+            while win_hsh.empty?
+                num = num - 1
+                    win_hsh = hand_hsh.select {|key, value| value.length == num};
+                    win_arr = win_hsh[win_hsh.keys[0]]
+            end
+        
+            case num
+                    #selects high card to accompany four of a kind
+                when 4
+                    compile_hand win_arr,num,hand_hsh,1
+                    hand ="four of a kind"
+                    # determines if it is a full house or just 3 of a kind and assigns high cards accordingly 
+                when 3
+                    # if theres two sets of three remove one card to make a fullhouse
+                    hand = "fullhouse"
+                    if win_arr.length > 3
+                        
+                        win_arr.sort! {|x,y| y.number <=> x.number}
+                        win_arr.pop()
+                        
+                    else
+                        other_card_hsh = hand_hsh.select {|key, card_arr| card_arr.length == 2};
+                        
+                        if other_card_hsh.empty?
+                            win_arr = compile_hand win_arr,num,hand_hsh,2
+                            hand = "three of a kind"
+                            
+                        else
+                            pair_arr = []
+                            other_card_hsh.each_value {|card_arr| pair_arr = pair_arr + card_arr}     
+                            pair_arr.sort! {|x,y| y.number <=> x.number}
+                            while pair_arr.length > 2
+                                pair_arr.pop
+                            end
+                                win_arr = win_arr + pair_arr
+                        end   
+                    end
+                        
+                    # determines if its two pair or a single pair and assigns high cards accordingly
+                when 2
+                    # if theres 3 pairs remove the lowest denomination one
+                    if win_arr.length > 4
+                        win_arr.sort! {|x,y| y.number <=> x.number}
+                        win_arr.pop(1)
+                    end
+                    # if it is two pair one high card required
+                    if win_arr.length == 4
+                        ################################# needs its own method or lowest demonination pair wont be counted as high card
+                       win_arr = compile_hand win_arr,num,hand_hsh,1
+                       hand = "two pair"
+                     # if it is one pair three high cards required
+                    else
+                        win_arr = compile_hand win_arr,num,hand_hsh,3
+                        hand = "pair"
+                    end
+                    
+                # if there is no pair determine high card
+                when 1
+                    hand = "high card"
+                    win_arr = win_arr + determine_high_card(hand_hsh,5)
+            end
+        end
+            win_arr << hand
+            @best_hand[@usr] = win_arr
+    end
+        
+    
+    def remove_lowest_value_cards num
+        if win_arr.length > num
+                win_arr.sort! {|x,y| y.number <=> x.number}
+                win_arr.pop()
+        end
+    end
+    #
+    #def better_then_flush hand,new_hand, win_arr
+    #    unless hand == "flush"
+    #        @best_hand[@usr] = win_arr
+    #    end
+    #end
+    #
+     #after determining the the type of each card deciding the best hand the player can make called in calculate hand
+    def straight? arr
+        arr.sort! {|x,y| y.number <=> x.number}
+        r = 0
+        card_ace = Card.new(14,arr[arr.length-1].suit)
+        card = nil
+        str_arr = []
+        #if arr.include? 10 and arr.include? 11 and arr.include? 12 and arr.include? 13 and arr.include? 1
+        #    straight = true
+        arr.insert(0,card_ace)if arr[arr.length-1] == 1
+            arr.each do |next_card|
+                unless card.nil?
+                    unless r == 5
+                        if card.number - 1 == next_card.number 
+                            r = r + 1
+                            str_arr << card
+                        else
+                            str_arr.clear
+                            r = 1
+                            str_arr << card
+                        end
+                    end
+                else
+                    r = 1
+                    card = next_card
+                    str_arr << card
+                end
+            end
+        @straight = true if r == 5
+        if str_arr.include? card_ace
+            str_arr.slice!(0)
+            str_arr.insert(0,arr[arr.length-1])
+        end
+    end
+    
+    def compile_hand win_arr,num,hand_hsh,high_cards
+        other_card_hsh = hand_hsh.select {|key, value| value.length < num};
+        win_arr = win_arr + determine_high_card(other_card_hsh,high_cards)
+    end
+    
+   # determines if highcards are needed which cards should be in the winning hand called in called in get_hand
+    def determine_high_card hsh,num
+        arr = []
+        num1 = 2
+
+        hsh.each_value {|card| arr << card}
+        arr.flatten!
+        arr.sort! {|x,y| y.number <=> x.number}
+        while arr.length > num
+            arr.pop
+        end
+        return arr
+    end
     
     # assigns cards to hash based on there number value called in calculate hand
     def determine_cards(card,hsh)
-        case card[0]
+        case card.number
         when 1
             assign_to_hsh(hsh,card,:ace)
         when 2
@@ -137,131 +308,19 @@ module PokergameHelper
         when 13
             assign_to_hsh(hsh,card,:king)
         end
-        
-        case card[1]
-        
-        when 'd'
-            assign_to_hsh(hsh,d,card,:diomand)
-        when 's'
-            assign_to_hsh(hsh,s,card,:spade)
-        when 'h'
-            assign_to_hsh(hsh,h,card,:heart)
-        when 'c'
-            assign_to_hsh(hsh,c,card,:club)
-        end
     end
     
-    # common method for assigning both card value and suit to hashes. called in determine suit, determine facevalue
-    def assign_to_hsh hsh,card,key 
-        hsh[key] = [] unless hsh.has_key? key
-        hsh[key] << card
-    end
-    
-    # after determining the the type of each card deciding the best hand the player can make called in calculate hand
-    def get_hand hand_hsh
-        hand = ""
-        get_best_hand hand_hsh,hand  
-    end
-    
-    def straight? hsh
-        arr = []
-        str_arr = []
-        hsh.each do |key,value|
-            value.each do |card|
-                arr << card
-            end
-        end
-        arr.sort! {|x,y| y[0] <=> x[0]}
-        a.selec
-        r = 0
-        if arr.include? 10 and arr.include? 11 and arr.include? 12 and arr.include? 13 and arr.include? 1
-            straight = true
-        else
-            arr.each do |card,next_card|
-                unless r == 5
-                    if card[0] - 1 == next_card[0]
-                        r + 1
-                        str_arr << card
-                    else
-                        r = 1
-                    endla
-                end
-            end
-        end
-        straight = true if r == 5
-        straight,str_arr
-    end
-    
-    
-    def get_best_hand hand_hsh,hand
-        win_arr = []
-        num = 5
-        win_hsh = hand_hsh.select {|key, value| value.length == num};
-        unless win_hsh.empty?
-           win_arr = win_hsh[win_hsh.keys[0]]
-        while win_hsh.empty?
-            num = num - 1
-            win_hsh = hand_hsh.select {|key, value| value.length == num};
-        end
-        case num
-        #selects high card to accompany four of a kind
-        when 4
-            other_card_hsh = hand_hsh.select {|key, value| value.length < 4};
-            win_hsh.merge(determine_high_card(other_card_hsh,1))
-            hand = "four of a kind"
-            # determines if it is a full house or just 3 of a kind and assigns high cards accordingly 
-        when 3
-            other_card_hsh = hand_hsh.select {|key, value| value.length == 2};
-            if other_card_hsh.empty?
-                other_card_hsh = hand_hsh.select {|key, value| value.length == 1};
-                win_hsh.merge(determine_high_card(other_card_hsh,3))
-                better_then_flush win_hsh, hand, "three of a kind"
-            end
-            win_hsh.merge(other_card_hsh)
-            hand = "fullHouse"
-            # determines if its two pair or a single pair and assigns high cards accordingly
-        when 2    
-        # if it is two pair one high card required
-            if win_hsh.length == 4
-                
-                other_card_hsh = hand_hsh.select {|key, value| value.length < 2};
-                win_hsh.merge(determine_high_card(other_card_hsh,1))
-                better_then_flush win_hsh, hand, "two pair"
-                # if it is one pair three high cards required
-            else
-                
-                other_card_hsh = hand_hsh.select {|key, value| value.length < 2};
-                win_hsh.merge(determine_high_card(other_card_hsh,3))
-                 better_then_flush win_hsh, hand, "pair"
-            end   
-        
-        # if there is no pair determine high card
-        when 1
-            win_hsh.merge(determine_high_card(hand_hsh,5))
-            better_then_flush win_hsh, hand, "high card"
-        end
-        return hand,win_hsh
-    end
-    
-    def better_then_flush hsh,hand,new_hand
-        unless hand == "flush"
-            hand = new_hand
-            hsh.each do |key, value|
-                value.each do |card|
-                    @best_hand[@usr] << card
-                end
-            end
+    def determine_suits(card,hsh)
+        case card.suit
+            
+            when 'd'
+                assign_to_hsh(hsh,card,:diomand)
+            when 's'
+                assign_to_hsh(hsh,card,:spade)
+            when 'h'
+                assign_to_hsh(hsh,card,:heart)
+            when 'c'
+                assign_to_hsh(hsh,card,:club)
         end
     end
-    
-    
-   # determines if highcards are needed which cards should be in the winning hand called in called in get_hand
-    def determine_high_card hsh,num
-        arr = []
-        num1 = 2
-        while hsh.length > num
-            hsh.delete_if {|key,value| value[0][0] == num1}
-            num1 = num1 + 1
-        end
-    end 
 end
